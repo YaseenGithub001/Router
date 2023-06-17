d85da0cc520e962f8066afb544708be08cf3c55e
Yaseeeen <halfduplex001@gmail.com>
2023-06-17 14:04:55 +0530

    1commit

---
+++ src/Pages/NotFound.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './NotFound.css'; 
+export default function NotFound() {
+  return (
+    <div>
+
+<h1><a href="#">404</a></h1>
+<p>There's nothing here.</p>
+<div class="travolta"></div>
+    
+    </div>
+  )
+}
+
+
+
